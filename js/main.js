7291d25fd7a497d9a2f18e75bc113bf8cfe8e20c
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-05-27 23:20:06 -0500

    Local storage implemented. Better User experience

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ NUM_CAMPOS=3;
 MIN_GOOD_AVERAGE = 4.3;
 MIN_EXCELENT_AVERAGE = 4.5;
 MAX_GRADE = 5;
-STORAGE = [];
+
 function newSubject(e){
     var autofocus = "";
     if (e.type == "click"){autofocus="autofocus";}
@@ -22,13 +22,13 @@ function calculate(e){
     var nprod = res.length;
     var cont = 0;
     $("#result").empty();//limpiar la caja de notas
-    STORAGE = []
+    var STORAGE = []
     var materia = [];//array para cada una de las materias
     var promedio = 0;
     var totalCreditos = 0;
+    var calc_str = "";
     for (i=0;i<nprod;i++){
         var val = (res[i].value).replace(",",".");
-        console.log(res[i].value,val);
         $("#result").append(val);
         materia.push(val); //esta linea agrega cada dato de la materia a un array
         if(cont < NUM_CAMPOS-1 ){
@@ -36,9 +36,9 @@ function calculate(e){
         }
         else{
             promedio += materia[1] * materia[2];
+            calc_str += materia[1] + " * " + materia[2] + " = " + (materia[1] * materia[2]) + " +<br>";
             totalCreditos += parseInt(materia[2]);
             // console.log("Total multiplicacion =" + promedio + " cred: "+ totalCreditos);
-            console.log(materia)
             STORAGE.push(materia)
             materia = []; // borramos el array
             cont=0;
@@ -47,22 +47,22 @@ function calculate(e){
     var total = "..."
     if (! isNaN(promedio/totalCreditos)){
         total = (promedio/totalCreditos).toFixed(1);
+        calc_str += "_______________<br>total = " + promedio+"<br>tot. creditos = " + totalCreditos + "<br><br>" + promedio +"/"+totalCreditos + " = " + (promedio/totalCreditos);
     }
     $("#result").text(total).removeClass("white");
-    saveStorage();
-    return message();
+    saveStorage(STORAGE);
+    return message(calc_str);
 }
-function saveStorage () {
+function saveStorage (STORAGE) {
     if(typeof(Storage)!=="undefined"){
         localStorage.setItem('notas', JSON.stringify(STORAGE));
     }
 }
-function restartStorage () {
+function loadStorage () {
     if(typeof(Storage)!=="undefined"){
         var backup = JSON.parse(localStorage.getItem('notas'));
         if (backup != null) {
             materias = []
-            console.log(backup.length)
             var i = 0;
             for (i; i<backup.length;i++){
                 materias.push({"nombre": backup[i][0], "nota": backup[i][1], "creditos": backup[i][2], "autofocus": ""})
@@ -81,7 +81,6 @@ function calculateGif(e){
 	var v = calculate(e);
     goToByScroll("#message");
     if( v >= MIN_EXCELENT_AVERAGE && v <= MAX_GRADE){
-        // $("#messageGif").html("<img src='http://data3.whicdn.com/images/47754974/tumblr_mfwost4yz91s06rcqo1_500_large.gif' />");
         $("#messageGif").html("<img src='img/gif1.gif' class='img-thumbnail congratulations' />").fadeIn("slow");
         $("#result").addClass("white");
     }
@@ -98,7 +97,8 @@ function removeSubject(e){
      * por esta razon se debe buscar el tr para eliminarlo.
      */
 }
-function message(){
+function message(calc){
+    $("#showCalc div").html(calc)
     $("#message, #messageGif").empty();
     var val = parseFloat($("#result").text());
     if( val >= MIN_GOOD_AVERAGE){
@@ -112,22 +112,40 @@ function message(){
     }
     return val;
 }
+function validateInput (e) {
+    var value = $(this).val();
+    console.log(e.keyCode, value);
+    $(this).val(value.replace(/\,/i, '.'));
+
+    if (!(/^[0-9.,]*$/.test(value))){
+        e.preventDefault();
+        $(this).val(value.slice(0,-1));
+    }
+    if (e.keyCode === 13){
+        calculateGif(e);
+    }
+}
+function showCalc(e) {
+    e.preventDefault();
+    $("#showCalc div").slideToggle();
+}
 function main(){
-    if(!restartStorage()){
+    if(!loadStorage()){
     	newSubject(jQuery.Event("keypress")); // simulamos un evento que no sea un click
     }
     $("input").addClass("form-control");
     $("#tablaUsuarios .grade:first").focus();
     $("#guardar").on("click", calculateGif);
+    $("#showCalc a").on("click", showCalc);
 }
 function goToByScroll(element, callback){
     $('html,body').animate({
         scrollTop: $(element).offset().top - 100},
         'slow', callback);
 }
-$(document).on("ready", main); // cuando el document esté 'ready' ejecutar la funcion 'main'
+$(document).ready(main); // cuando el document esté 'ready' ejecutar la funcion 'main'
 
-// $(document).on("keyup", "input", calculate);
+$(document).on("keyup", 'input', validateInput);
 $(document).on("click", ".new-subject", newSubject);//si dan click a .newp ejecutar nuevo
 $(document).on("click", ".remove-subject", removeSubject);//si dan click a .newp ejecutar nuevo
 
